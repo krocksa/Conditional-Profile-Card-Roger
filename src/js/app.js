7bcb20d5168f76fc61b0d079e8f1a19b98acf087
krocksa <rogeryonatan@gmail.com>
2022-09-19 02:07:08 +0000

    Conditional Profile Card Listerine

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "Roger" : variables.name}, ${
+    variables.lastname === null ? "Salgado" : variables.lastname
+  } </h1>
+          <h2>${
+            variables.role === null
+              ? "Estudiante De 4Geeks y CIAP"
+              : variables.role
+          }</h2>
+          <h3>${variables.city === null ? "Caracas" : variables.city}, ${
+    variables.country === null ? "Venezuela" : variables.country
+  } </h3>
+          <ul class="${
+            variables.socialMediaPosition === "position-left"
+              ? "position-left"
+              : "position-right"
+          } ">
+          <ul class="position-right">           
+          <li><a href="https://twitter.com/${variables.twitter}">
+            <i class="fab fa-twitter"></i></a></li>         
+          <li><a href="https://github.com/${variables.github}">
+          <i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}">
+            <i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}">
+            <i class="fab fa-instagram"></i></a></li>
           </ul>
+         
+          
         </div>
     `;
 }
@@ -56,12 +75,13 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://th.bing.com/th/id/OIP.zSOb1aoiAGJZpOdU9MplMgHaHa?pid=ImgDet&rs=1",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
